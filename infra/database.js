a43bcce0104b36e73143c772f780f62791345db5
Alexandre Soares <104173218+allesoares95@users.noreply.github.com>
2024-09-22 14:00:34 +0000

    add  and  files

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+// This function is used to query the database
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+// This object is exported to be used in other files
+export default {
+  query: query,
+};
