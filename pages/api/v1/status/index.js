36ac555b36a065b6263b94844a5211af29087602
Alexandre Soares <104173218+allesoares95@users.noreply.github.com>
2024-09-23 20:36:40 +0000

    finish /api/v1/status finish

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "test ok" });
+  const updateAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version");
+  const postgresVersion = databaseVersion.rows[0].server_version;
+
+  const connectionsMax = await database.query("SHOW max_connections");
+  const maxConnections = connectionsMax.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const connectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const connectionsOpen = connectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        open_connections: connectionsOpen,
+      },
+    },
+  });
 }
 
 export default status;
